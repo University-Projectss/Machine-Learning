82e26257c2e16bc36d0509df5b013bd940080c62
rob3rtu <robertudrea2002@yahoo.ro>
2023-07-01 20:24:53 +0300

    finish cars/road logic

--- Self-Drive Car/index.js
+++ Self-Drive Car/index.js
@@ -3,12 +3,16 @@ canvas.width = 200;
 
 const ctx = canvas.getContext("2d");
 const road = new Road(canvas.width / 2, canvas.width * 0.9);
-const car = new Car(road.getLaneCenter(1), 200, 30, 50);
+const car = new Car(road.getLaneCenter(1), 100, 30, 50, "KEYS");
+const traffic = [new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 2)];
 
 animate();
 
 function animate() {
-  car.update(road.borders);
+  for (let i = 0; i < traffic.length; i++) {
+    traffic[i].update(road.borders, []);
+  }
+  car.update(road.borders, traffic);
 
   //this will erase the canvas
   canvas.height = window.innerHeight;
@@ -16,7 +20,10 @@ function animate() {
   ctx.save();
   ctx.translate(0, canvas.height * 0.7 - car.y);
   road.draw(ctx);
-  car.draw(ctx);
+  for (let i = 0; i < traffic.length; i++) {
+    traffic[i].draw(ctx, "black");
+  }
+  car.draw(ctx, "blue");
 
   ctx.restore();
   //this calls the function many times to give the animation effect
